7fdebe3534cca71f20f6a852696354f18f667458
Samuel Pastor <samuelpastorpinillos@gmail.com>
2023-02-22 17:29:32 +0100

    acabado 2

--- src/activitat18/Menu.java
+++ src/activitat18/Menu.java
@@ -63,9 +63,6 @@ public class Menu {
                     System.out.println("Adiós");
                     return;
 
-                default:
-                    System.out.println("¡Error! La opción seleccionada no es válida");
-
             }
         }while (true);
 
